c99c7d23fbceb459169f4209c0631d237fac741a
Tejas021 <tejaskolwankar021@gmail.com>
2021-12-24 15:36:53 +0530

    last

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Header from './components/Header'
-import Nav from './components/Nav'
-import Result from './components/Result'
-import {requests} from './utils/requests'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Result from '../components/Result'
+import {requests} from '../utils/requests'
 export default function Home(props) {
   return (
     <div className="text-gray-400">
